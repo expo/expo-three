02145b9219869e063403ed7d7de6a02ba8dfa24d
Evan Bacon <baconbrix@gmail.com>
2018-06-21 10:00:35 -0700

    fully pushed iOS build 1

--- example/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ example/android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -24,9 +24,12 @@ public class AppConstants {
   static {
     List<Constants.EmbeddedResponse> embeddedResponses = new ArrayList<>();
 
-    // ADD EMBEDDED RESPONSES HERE
-    // START EMBEDDED RESPONSES
-    // END EMBEDDED RESPONSES
+    
+        // ADD EMBEDDED RESPONSES HERE
+        // START EMBEDDED RESPONSES
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@bacon/expo-three/index.exp", "assets://shell-app-manifest.json", "application/json"));
+        embeddedResponses.add(new Constants.EmbeddedResponse("https://d1wp6m56sqw74a.cloudfront.net/%40bacon%2Fexpo-three%2F1.0.0%2F0e8ada5ffc256df55f6e0eb4cd41b42d-28.0.0-android.js", "assets://shell-app.bundle", "application/javascript"));
+        // END EMBEDDED RESPONSES
     EMBEDDED_RESPONSES = embeddedResponses;
   }
 
