3b263c7b4200592f621061a7e52122db46c4c309
Evan Bacon <baconbrix@gmail.com>
2019-08-13 15:58:57 -0700

    Added example project

--- src/Three.ts
+++ src/Three.ts
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
 import suppressExpoWarnings from './suppressWarnings';
+
+// @ts-ignore
+global.THREE = global.THREE || THREE;
 THREE['suppressExpoWarnings'] = suppressExpoWarnings;
 export default THREE;
