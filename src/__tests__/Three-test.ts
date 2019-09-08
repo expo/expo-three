489c06d60c393565924cca6f8146845de4e58b2f
Evan Bacon <baconbrix@gmail.com>
2019-09-08 15:10:29 -0700

    Added failing test

--- src/__tests__/Three-test.ts
+++ src/__tests__/Three-test.ts
@@ -1,23 +1,20 @@
-import '..';
-
 import { Platform } from '@unimodules/core';
 
-declare var THREE: any;
-declare var __expo_three_oldWarn: any;
+declare var global: {
+  THREE: any;
+  __expo_three_oldWarn: any;
+};
 
 it(`defines a global instance of three.js`, () => {
-  expect(THREE).toBeDefined();
-});
-
-it(`has a custom method for muting warnings`, () => {
-  expect(THREE.suppressExpoWarnings).toBeDefined();
-});
-
-it(`THREE.suppressExpoWarnings is invoked on import`, () => {
+  expect(global.THREE).toBeDefined();
+  // @ts-ignore
+  require('..');
+  expect(global.THREE).toBeDefined();
+  expect(global.THREE.suppressExpoWarnings).toBeDefined();
   if (Platform.OS === 'web') {
     // @ts-ignore
     expect(global.__expo_three_oldWarn).not.toBeDefined();
   } else {
-    expect(__expo_three_oldWarn).toBeDefined();
+    expect(global.__expo_three_oldWarn).toBeDefined();
   }
 });
