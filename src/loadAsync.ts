8a6c18380b5cb8c4af1012af18036886cb041be4
Evan Bacon <baconbrix@gmail.com>
2019-09-08 14:00:15 -0700

    test invoking Jest tests

--- src/loadAsync.ts
+++ src/loadAsync.ts
@@ -36,6 +36,7 @@ export default async function loadAsync(
   onProgress?: ProgressCallback,
   onAssetRequested = function() {},
 ) {
+  console.log('')
   let urls = await resolveAsset(res);
   if (!urls) {
     throw new Error(
