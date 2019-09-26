19c16c9e1c39a87a2f2c12542fdd79650c853c50
Evan Bacon <baconbrix@gmail.com>
2019-09-26 12:12:30 -0400

    Update App.tsx

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,18 @@
 import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
-import { Renderer, TextureLoader, THREE } from 'expo-three';
+import { Renderer, TextureLoader } from 'expo-three';
 import * as React from 'react';
+import {
+  AmbientLight,
+  BoxBufferGeometry,
+  Fog,
+  GridHelper,
+  Mesh,
+  MeshStandardMaterial,
+  PerspectiveCamera,
+  PointLight,
+  Scene,
+  SpotLight,
+} from 'three';
 
 export default function App() {
   let timeout;
@@ -18,52 +30,35 @@ export default function App() {
         const sceneColor = 0x6ad6f0;
 
         // Create a WebGLRenderer without a DOM element
-        const renderer = new Renderer({ gl, antialias: true });
+        const renderer = new Renderer({ gl });
         renderer.setSize(width, height);
         renderer.setClearColor(sceneColor);
 
-        const camera = new THREE.PerspectiveCamera(
-          70,
-          width / height,
-          0.01,
-          1000,
-        );
-        camera.position.set(2, -5, 5);
+        const camera = new PerspectiveCamera(70, width / height, 0.01, 1000);
+        camera.position.set(2, 5, 5);
 
-        const scene = new THREE.Scene();
-        scene.fog = new THREE.Fog(sceneColor, 1, 10000);
-        scene.add(new THREE.GridHelper(10, 10));
+        const scene = new Scene();
+        scene.fog = new Fog(sceneColor, 1, 10000);
+        scene.add(new GridHelper(10, 10));
 
-        const ambientLight = new THREE.AmbientLight(0x101010);
+        const ambientLight = new AmbientLight(0x101010);
         scene.add(ambientLight);
 
-        const pointLight = new THREE.PointLight(0xffffff, 2, 1000, 1);
+        const pointLight = new PointLight(0xffffff, 2, 1000, 1);
         pointLight.position.set(0, 200, 200);
         scene.add(pointLight);
 
-        const spotLight = new THREE.SpotLight(0xffffff, 0.5);
+        const spotLight = new SpotLight(0xffffff, 0.5);
         spotLight.position.set(0, 500, 100);
         spotLight.lookAt(scene.position);
         scene.add(spotLight);
 
-        const gem = new GemMesh();
-        gem.position.set(0, 1, 0);
-        scene.add(gem);
-
-        camera.lookAt(gem.position);
-
         const cube = new IconMesh();
-        cube.position.x = 2;
         scene.add(cube);
 
-        // Setup camera controls (WEB ONLY)
-        require('three/examples/js/controls/OrbitControls');
-        // @ts-ignore
-        new THREE.OrbitControls(camera);
+        camera.lookAt(cube.position);
 
         function update() {
-          gem.rotation.y += 0.1;
-
           cube.rotation.y += 0.05;
           cube.rotation.x += 0.025;
         }
@@ -81,34 +76,13 @@ export default function App() {
   );
 }
 
-class GemMesh extends THREE.Mesh {
-  constructor() {
-    const geometry = new THREE.CylinderGeometry(0.6, 1, 0.3, 8, 1);
-    geometry.vertices[geometry.vertices.length - 1].y = -1;
-    geometry.verticesNeedUpdate = true;
-
-    super(
-      geometry,
-      new THREE.MeshPhongMaterial({
-        color: 0x37be95,
-        wireframe: false,
-        side: THREE.DoubleSide,
-        flatShading: true,
-        refractionRatio: 0.5,
-        opacity: 0.7,
-        transparent: true,
-        shininess: 3100,
-      }),
-    );
-  }
-}
-
-class IconMesh extends THREE.Mesh {
+class IconMesh extends Mesh {
   constructor() {
     super(
-      new THREE.BoxBufferGeometry(1.0, 1.0, 1.0),
-      new THREE.MeshStandardMaterial({
+      new BoxBufferGeometry(1.0, 1.0, 1.0),
+      new MeshStandardMaterial({
         map: new TextureLoader().load(require('./assets/icon.png')),
+        // color: 0xff0000
       }),
     );
   }
